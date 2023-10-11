ea3163bbdb69821288b10a333c1a655de0f83656
mustafaahmedd <mustafaa.irshad@gmail.com>
2023-10-12 02:34:03 +0500

    Updated App.jsx

--- React/App.jsx
+++ React/App.jsx
@@ -1,57 +1,104 @@
 import reactLogo from './assets/react.svg'
-import './App.css'
 import React from 'react';
-import { ReactDOM } from 'react';
+import ReactDOM  from 'react-dom/client';
 import './css/style.css'
+import './App.css'
+import './index.css'
 
 function App() {
   const [input, setInput] = React.useState("")
   const [output, setOutput] = React.useState(0)
-  const operator = ['+','-','*','/'];
+  const [equals, setEquals] = React.useState(false)
+  const operators = ['+','-','*','/'];
 
-  const handleButton = value => {
-      if(value === '0' && input[input.length - 1] === '0')
-         noMultipleZeros();
-      else if(value === '.'){
-          if(input === ''){
-              setInput('0.')
-              setOutput('0.')
-          }
-          else
-              noMultipleDecimals(value);
-      }
-      else{
-          setInput((prevInput) => prevInput + value)
-          setOutput(value)
-      }
+  const handleNumbers = (val) => {
 
+    if(equals)
+      reset();
+    if(val === '0'){
+      const parts = input.split(/([+\-*/])/);
+      let lastPart = parts[parts.length - 1];
+
+      //starting case
+      if(input === '0' || (lastPart[0] === '0' && !lastPart.includes('.') && !/^[1-9]$/.test(lastPart[1]) )) 
+        return; 
+      else
+        addDigit(val);
+    }
+    else
+      addDigit(val);
   }
-  const noMultipleZeros = () => setInput((prevInput) => prevInput)
 
-  const noMultipleDecimals = (value) => { 
-      if(input[input.length - 1] === '.')
-          return;
-      // else if(input.includes('.'))
-      //     setInput((prev) => prev)
-      else{
-          setInput((prevInput) => prevInput + value)
-          setOutput(value)
+  const addDigit = (val) => {
+    const temp = String(output);
+    setInput((prevInput) => prevInput + val);
+
+    if (temp === '0' || operators.some(operator => temp.includes(operator))) 
+      setOutput(val);
+    else
+        setOutput((prevOutput) => prevOutput + val);
+    /* setInput((prevInput) => prevInput + val)
+    (temp[0] === '0' || operators.some(operator => temp.includes(operator)))
+      ? setOutput(val) 
+        :  setOutput((prevOutput) => prevOutput + val) */
+  }
+  
+  const handleDecimal = () => {
+      // Split the input string based on operators
+    const parts = input.split(/([+\-*/])/);
+    const lastPart = parts[parts.length - 1];
+
+    /*(equals || input === '') 
+        ? (setInput('0.'), 
+          setOutput('0.'), 
+          setEquals(false), 
+          console.log("first decimal condition",equals)) 
+          : (!lastPart.includes('.')) 
+            ? (console.log("No decimal before"),
+              setInput((prevInput) => prevInput + '.'),
+              setOutput((prevOutput) => prevOutput + '.'))
+              : console.log("nothing worked"); */
+
+    if(equals || input === ''){
+      setInput('0.')
+      setOutput(0 + '.')
+      setEquals(false)
+    }
+    else{
+      if (!lastPart.includes('.')) {
+        console.log("No decimal in last digits")
+        setInput((prevInput) => prevInput + '.');
+        setOutput((prevOutput) => prevOutput + '.');
       }
+    }
+  }
+  
+  const handleOperators = (operator) => {
+    if(equals){
+      setInput(output + operator);
+      setOutput(operator)
+      setEquals(!equals)
+    }
+    else if(!equals){
+      setInput((prev) => prev + operator)
+      setOutput(operator)
+    }
   }
 
   const total = () => {
-      console.log(input)
-      const answer = eval(input)
-      // setInput((prevInput) => prevInput + "=" + answer)
-      // console.log(answer)
+      const answer = math.evaluate(input)
+      setInput((prevInput) => prevInput + "=" + answer)
       setOutput(answer)
-      setInput(answer)
+      setEquals(true)
   }
+
   const reset = () => {
-      setInput("");
-      setOutput(0);
+    setInput("");
+    setOutput(0);
+    setEquals(false);
   }
-  const clear = () => setOutput(setInput((prev) => prev.split("").slice(0, prev.length-1).join("")))
+
+  const clear = () => {setOutput(setInput((prev) => prev.split("").slice(0, prev.length-1).join("")))}
 
   return (
     <>
@@ -65,28 +112,28 @@ function App() {
             <div className="formulaScreen">{input}</div>
             <div className="output" id="display">{output}</div>
             <div className="grid">
-              <button className="padButton clear"id="clear" onClick={()=> reset()}>AC</button>
-              <button className="padButton back"id="backSpace" onClick={()=> clear()}>C</button>
-              <button className="padButton divide" id="divide" onClick={()=> handleButton('/')}>/</button>
-              <button className="padButton multiply" id="multiply" onClick={()=> handleButton('*')}>x</button>
+              <button className="padButton clear" id="clear" onClick= {reset} >AC</button>
+              <button className="padButton back" id="backSpace" onClick={()=> clear()}>C</button>
+              <button className="padButton divide" id="divide" onClick={()=> handleOperators('/')}>/</button>
+              <button className="padButton multiply" id="multiply" onClick={()=> handleOperators('*')}>x</button>
 
-              <button className="padButton seven darkGray" id="seven" onClick={()=> handleButton('7')}>7</button>
-              <button className="padButton eight darkGray" id="eight" onClick={()=> handleButton('8')}>8</button>
-              <button className="padButton nine darkGray" id="nine" onClick={()=> handleButton('9')}>9</button>
-              <button className="padButton subtract" id="subtract" onClick={()=> handleButton('-')}>-</button>
+              <button className="padButton seven darkGray" id="seven" value="7" onClick={()=> handleNumbers('7')}>7</button>
+              <button className="padButton eight darkGray" id="eight" value="8" onClick={()=> handleNumbers('8')}>8</button>
+              <button className="padButton nine darkGray" id="nine" value="9" onClick={()=> handleNumbers('9')}>9</button>
+              <button className="padButton subtract" id="subtract" onClick={()=> handleOperators('-')}>-</button>
 
-              <button className="padButton four darkGray" id="four" onClick={()=> handleButton('4')}>4</button>
-              <button className="padButton five darkGray" id="five" onClick={()=> handleButton('5')}>5</button>
-              <button className="padButton six darkGray" id="six" onClick={()=> handleButton('6')}>6</button>
-              <button className="padButton add" id="add" onClick={()=> handleButton('+')}>+</button>
+              <button className="padButton four darkGray" id="four" value="4" onClick={()=> handleNumbers('4')}>4</button>
+              <button className="padButton five darkGray" id="five" value="5" onClick={()=> handleNumbers('5')}>5</button>
+              <button className="padButton six darkGray" id="six" value="6" onClick={()=> handleNumbers('6')}>6</button>
+              <button className="padButton add" id="add" onClick={()=> handleOperators('+')}>+</button>
 
-              <button className="padButton one darkGray" id="one" onClick={()=> handleButton('1')}>1</button>
-              <button className="padButton two darkGray" id="two" onClick={()=> handleButton('2')}>2</button>
-              <button className="padButton three darkGray" id="three" onClick={()=> handleButton('3')}>3</button>
+              <button className="padButton one darkGray" id="one" value="1" onClick={()=> handleNumbers('1')}>1</button>
+              <button className="padButton two darkGray" id="two" value="2" onClick={()=> handleNumbers('2')}>2</button>
+              <button className="padButton three darkGray" id="three" value="3" onClick={()=> handleNumbers('3')}>3</button>
               <button className="padButton equals " id="equals" onClick={()=> total()}>=</button>
               
-              <button className="padButton zero darkGray" id="zero" onClick={()=> handleButton('0')}>0</button>
-              <button className="padButton decimal darkGray" id="decimal" onClick={()=> handleButton('.')}>.</button>
+              <button className="padButton zero darkGray" id="zero" value="0" onClick={()=> handleNumbers('0')}>0</button>
+              <button className="padButton decimal darkGray" id="decimal" onClick={()=> handleDecimal()}>.</button>
             </div>
         </div>
         <div>
@@ -98,4 +145,5 @@ function App() {
   )
 }
 
-export default App;
+// export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
